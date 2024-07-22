86a75eef920ca73424dee40f520e428c381571c8
Artem Vasenin <vasart169@gmail.com>
2024-07-22 23:24:00 +0100

    style: update ruff in pre-commit and format

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,17 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_id() -> str:
     return "class_cache.tests.id"
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_key() -> str:
     return "class_cache.tests.key"
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_value() -> list[str]:
     # Return a list to test that unhashable types can also be stored
     return ["class_cache.tests.value"]
